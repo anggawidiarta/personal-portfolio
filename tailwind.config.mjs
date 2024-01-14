03928b5a56d2dfcb8b8e858cb5158b19fe35fd41
anggawidiarta <anggawidiarta55@gmail.com>
2024-01-14 23:24:29 +0800

    feat: modifying all components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: { colors: { prussian: "#1D63FF" }, fontFamily: {  } },
+    extend: {
+      colors: { prussian: "#1D63FF" },
+    },
+    fontFamily: {
+      helvetica: ["Helvetica", "Arial", "sans-serif"],
+      libre: ["Libre Baskerville", "Garamond"],
+      fjalla: ["Fjalla One"],
+    },
   },
   plugins: [],
 };
